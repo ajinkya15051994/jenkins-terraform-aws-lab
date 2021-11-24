7193a8c9cdc6a4ec10617404815fc0588d2a754b
ajinkya15051994 <62056786+ajinkya15051994@users.noreply.github.com>
2021-11-24 18:16:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "s326092021-terraform-backend"
+    bucket = "terraform-ajinkya"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
